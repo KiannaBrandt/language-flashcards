e2fd71d8f4a37e1c481c760464f817a49fd71790
Uzair Ashraf <uzinatorcl@live.com>
2021-01-19 20:49:20 -0800

    initial commit

---
+++ data.js
@@ -0,0 +1,22 @@
+// Do not write any code here
+
+var data = {
+  cardId: 4,
+  products: [
+    {
+      id: 1,
+      question: 'dónde está la biblioteca',
+      answer: 'Where is the library?'
+    },
+    {
+      id: 2,
+      question: 'Buenas noches',
+      answer: 'Good evening'
+    },
+    {
+      id: 3,
+      question: 'Yo no comprendo',
+      answer: 'I do not understand'
+    }
+  ]
+}
